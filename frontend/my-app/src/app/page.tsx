35344f8ee42b103c57b2eb755091bc4332df3bc2
Nada <mo@Nadas-MacBook-Air-2.local>
2024-12-01 13:00:37 +0200

    Handled conflicts in merging, added documentation files

--- frontend/my-app/src/app/page.tsx
+++ frontend/my-app/src/app/page.tsx
@@ -1,28 +1,21 @@
-<<<<<<< HEAD
 import Image from "next/image";
 import ProjectCard from '../pages/Project';
 import styles from "./page.module.css";
-=======
 import React from 'react';
 import LoginForm from '../pages/Login/LoginForm';
->>>>>>> 3db4b8bc139d3a32701bb9bc8b7c7297a5219b8a
 
 
 
 export default function Home() {
-<<<<<<< HEAD
   return (
     <div className={styles.page}>
       <h1>My Projects</h1>
       <div className={styles.projectsContainer}>
-        {/* Dynamically passing project number */}
         <ProjectCard projectId={1} />
         <ProjectCard projectId={2} />
         <ProjectCard projectId={3} />
       </div>
     </div>
   );
-=======
   return <LoginForm />;
->>>>>>> 3db4b8bc139d3a32701bb9bc8b7c7297a5219b8a
 }
--- frontend/my-app/src/components/ui/Button.jsx
+++ frontend/my-app/src/components/ui/Button.jsx
@@ -1,19 +1,16 @@
 import React from 'react';
-<<<<<<< HEAD
+import './Button.css';
 
-const Button = ({ onClick, label }) => (
-  <button onClick={onClick}>{label}</button>
-);
-=======
-import './Button.css'; 
-
-const Button = ({ children, type = 'button' }) => {
+const Button = ({ onClick, children, type = 'button', className = '' }) => {
   return (
-    <button type={type} className="button">
+    <button
+      type={type}
+      className={`button ${className}`}
+      onClick={onClick}
+    >
       {children}
     </button>
   );
 };
->>>>>>> 3db4b8bc139d3a32701bb9bc8b7c7297a5219b8a
 
 export default Button;
