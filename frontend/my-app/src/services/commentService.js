331c6069929f439d7f096e0c98ae20cffe502d18
Sammany1 <ialsammany@gmail.com>
2024-12-24 03:00:16 +0200

    Remove unused services and comment out Duplicated ToDoPage component code

--- frontend/my-app/src/services/commentService.js
+++
@@ -1,8 +0,0 @@
-import { apiClient } from '../utils/apiClient';
-
-export const commentService = {
-  getComments: () => apiClient.get('/comments/'),
-  createComment: (commentData) => apiClient.post('/comments/', commentData),
-  updateComment: (commentId, commentData) => apiClient.put(`/comments/${commentId}/`, commentData),
-  deleteComment: (commentId) => apiClient.delete(`/comments/${commentId}/`),
-};
